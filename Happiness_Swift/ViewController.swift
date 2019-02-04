856aa927713a83fc3ebcc8f71f91cceb0b5866e6
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-02-04 18:38:44 +0900

    Initial Commit

---
+++ Happiness_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happiness_Swift
+//
+//  Created by JihoonPark on 04/02/2019.
+//  Copyright © 2019 JihoonPark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
